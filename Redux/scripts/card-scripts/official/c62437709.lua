a3e67d45671c0496a10b746d59f0f64ba8b1fb69
White Goose <17803402+WhiteG00se@users.noreply.github.com>
2026-07-03 23:29:59 +0200

    Bugfix Alien Gray, draw shouldn't be optional

--- Redux/scripts/card-scripts/official/c62437709.lua
+++ Redux/scripts/card-scripts/official/c62437709.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DRAW)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetTarget(s.drtarget)
@@ -32,7 +32,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 function s.drtarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
