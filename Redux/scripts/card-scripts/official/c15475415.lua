7fe879fff59c91e6da6faf3068e2690e8eb70169
White Goose <17803402+WhiteG00se@users.noreply.github.com>
2026-07-07 14:26:21 +0200

    Alien Dog Text Improvement

--- Redux/scripts/card-scripts/official/c15475415.lua
+++ Redux/scripts/card-scripts/official/c15475415.lua
@@ -21,19 +21,6 @@ function s.initial_effect(c)
 	e2:SetTarget(s.cttg)
 	e2:SetOperation(s.ctop)
 	c:RegisterEffect(e2)
-	--ATK/DEF loss for monsters with A-Counters battling "Alien" monsters
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_UPDATE_ATTACK)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e3:SetCondition(s.adcon)
-	e3:SetTarget(s.adtg)
-	e3:SetValue(s.adval)
-	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_UPDATE_DEFENSE)
-	c:RegisterEffect(e4)
 end
 s.listed_series={SET_ALIEN}
 s.counter_place_list={COUNTER_A}
@@ -67,14 +54,4 @@ function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		g:GetFirst():AddCounter(COUNTER_A,1)
 	end
-end
-function s.adcon(e)
-	return Duel.IsPhase(PHASE_DAMAGE_CAL) and Duel.GetAttackTarget()
-end
-function s.adtg(e,c)
-	local bc=c:GetBattleTarget()
-	return bc and c:GetCounter(COUNTER_A)~=0 and bc:IsSetCard(SET_ALIEN)
-end
-function s.adval(e,c)
-	return c:GetCounter(COUNTER_A)*-300
 end
